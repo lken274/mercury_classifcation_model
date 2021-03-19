45c4cd089352836fa4d36bf68d21c68e9ce60aee
Logan Kenwright <Logan.Kenwright@gmail.com>
2021-03-19 14:16:57 +1300

    added frame dump

--- run_video_model.py
+++ run_video_model.py
@@ -2,11 +2,16 @@ import glob
 import cv2
 import numpy as np
 import time
+import tensorflow as tf
+from inferenceutils import *
 
 video_dir = '/home/logan/Desktop/tf_models/blemish_detector/evaluation_videos/sept_2020_class3_dark_blemish/*.jpg'
-window_name = 'image'
+model_dir = 'inference_graph/saved_model'
+labelmap_path = 'dataset/labelmap.pbtxt'
+save_video = True
+
 render_scale = 0.25 #resolution rescaling during edge detection to improve performance
-skip_frames = 100 #start at a later point in the video so we aren't waiting for fruit
+skip_frames = 0 #start at a later point in the video so we aren't waiting for fruit
 low_Hue, low_Sat, low_Val = 10, 80, 95
 high_Hue, high_Sat, high_Val = 70, 255, 255
 light_fruit_colour = (high_Hue,high_Sat,high_Val)
@@ -14,13 +19,25 @@ dark_fruit_colour = (low_Hue,low_Sat,low_Val)
 
 minFruitSize = 10000 * render_scale
 minAspectRatio = 0.5
+detection_threshold = 0.35
 
 def main():
     #load video from file
     frames = [cv2.imread(file) for file in sorted(glob.glob(video_dir))]
     frames = frames[skip_frames:]
+    numFrames = len(frames-skip_frames)
+    size = (frames[0].shape[1], frames[0].shape[0])
+    print(size)
+    category_index = label_map_util.create_category_index_from_labelmap(labelmap_path, use_display_name=True)
+    model = tf.saved_model.load(model_dir)
 
-    for roiFrame in frames:
+    if (save_video == True):
+        out_vid = cv2.VideoWriter('demo.mp4',cv2.VideoWriter_fourcc(*'DIVX'), 10, (size))
+   
+
+    for idx,roiFrame in enumerate(frames):
+        print("Frame " + str(idx) + " of " + numFrames)
+        tf.keras.backend.clear_session()
         roi_resized = cv2.resize(roiFrame, (round(render_scale*roiFrame.shape[1]), round(render_scale*roiFrame.shape[0])))
         frame_HSV = cv2.cvtColor(roi_resized, cv2.COLOR_BGR2HSV) #convert to HSV
         frame_blur = cv2.bilateralFilter(frame_HSV, 5, 100, 100)
@@ -28,7 +45,7 @@ def main():
         edges = cv2.Canny(frame_threshold, 200, 250)
         contours = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]#find contours
         
-        fruitHulls = []
+        cnn_outputs = []
         for contour in contours:
             area = cv2.contourArea(contour, False) #get contouring area to cull bad contours
             if (area < minFruitSize):
@@ -50,7 +67,6 @@ def main():
                 point[0] = round((point[0] - x) / render_scale)
                 point[1] = round((point[1] - y) / render_scale)
 
-            fruitHulls.append((sparseConvexHull, (x,y,w,h)))
             #create a mask based on hull contours, (drawContours) [fruitHullMask]
             origY, origH = round(y / render_scale), round(h / render_scale)
             origX, origW = round(x / render_scale), round(w / render_scale)
@@ -60,16 +76,34 @@ def main():
             cv2.drawContours(mask, [sparseConvexHull],-1,(255,255,255), cv2.FILLED)
             img2gray = cv2.cvtColor(mask,cv2.COLOR_BGR2GRAY)
             maskedImage = cv2.bitwise_and(fruitCropBGR, fruitCropBGR, mask=img2gray)
-            cv2.imshow('cropped', fruitCropBGR)
-            cv2.imshow('outlined', img2gray)
-            cv2.imshow('masked', maskedImage)
-            cv2.waitKey(100)
-    #get list of every fruit and their original coordinates
 
-    #run classifier on each fruit in frame
+            output_dict = run_inference_for_single_image(model, maskedImage)
 
-    #draw original frame
-    #place bounding boxes on original image
+            boxes = output_dict['detection_boxes']
+            #translate coordinate system from percentage of masked image to absolute full image
+            for idx, pos in enumerate(boxes): 
+                ymin = pos[0] * origH + origY
+                ymax = pos[2] * origH + origY
+                xmin = pos[1] * origW + origX
+                xmax = pos[3] * origW + origX
+                boxes[idx] = [ymin, xmin, ymax, xmax]
+
+            cnn_outputs.append(output_dict)
+            vis_util.visualize_boxes_and_labels_on_image_array(
+            roiFrame,
+            output_dict['detection_boxes'],
+            output_dict['detection_classes'],
+            output_dict['detection_scores'],
+            category_index,
+            instance_masks=output_dict.get('detection_masks_reframed', None),
+            use_normalized_coordinates=False,
+            line_thickness=3,
+            min_score_thresh=detection_threshold)
+
+        if (save_video == True):
+            out_vid.write(roiFrame)
+    if(save_video == True):
+        out_vid.release()
 
 def createSparseHull(contour, stride):
     if (stride == 1):
