a6b4b5def36e4d20381fb807af93f65b1abe5fdc
Logan Kenwright <Logan.Kenwright@gmail.com>
2021-04-29 18:30:56 +1200

    replaced nms algorithm to be confidence weighted

--- run_video_model.py
+++ run_video_model.py
@@ -36,9 +36,10 @@ dark_fruit_colour = (low_Hue,low_Sat,low_Val)
 
 minFruitSize = 10000 * render_scale
 minAspectRatio = 0.5
-detection_threshold = 0.8
+detection_threshold = 0.75
+nonmax_sigma = 0.1
 
-num_boxes_per_frame = 20
+num_boxes_per_frame = 40
 
 num_batch_frames = 1
 
@@ -160,7 +161,8 @@ def main():
                         list(chunks(classes, num_boxes_per_frame)), list(chunks(scores, num_boxes_per_frame)))
 
         for idx, pos in enumerate(all_fruit_data[0]): #iterate through each fruit on screen
-            b,s,c = cull_below_threshold(all_fruit_data[0][idx], all_fruit_data[2][idx], all_fruit_data[1][idx], detection_threshold)
+            #b,s,c = cull_below_threshold(all_fruit_data[0][idx], all_fruit_data[2][idx], all_fruit_data[1][idx], detection_threshold)
+            b,s,c = all_fruit_data[0][idx], all_fruit_data[2][idx], all_fruit_data[1][idx]
             for defectidx,defectpos in enumerate(b):
                 bbox = boundingBoxes[idx]
                 ymin = defectpos[0] * bbox[3] + bbox[1]
@@ -170,12 +172,15 @@ def main():
 
                 b[defectidx] = [ymin, xmin, ymax, xmax]
 
-            b,s,c = non_max_suppression_fast(np.array(b),np.array(s),np.array(c), 0.6)
+            b,s,c = np.array(b), np.array(s), np.array(c, dtype=np.uint8)
+            keepIndexes = py_cpu_softnms(b,s,sigma=nonmax_sigma, thresh=detection_threshold)
+            b,s,c = b[keepIndexes],s[keepIndexes],c[keepIndexes]
+
             vis_util.visualize_boxes_and_labels_on_image_array(
             roiFrame,
-            np.array(b),
-            np.array(c, dtype=np.uint8),
-            np.array(s),
+            b,
+            c,
+            s,
             category_index,
             instance_masks=masked_reframed,
             use_normalized_coordinates=False,
@@ -223,51 +228,88 @@ def cull_below_threshold(boxes, scores, classes, thresh):
             new_classes.append(classes[idx])
     return (new_boxes, new_scores, new_classes)
 
-def non_max_suppression_fast(boxes, scores, classes, overlapThresh):
-    # if there are no boxes, return an empty list
-    if len(boxes) == 0:
-        return ([],[],[])
-    # if the bounding boxes integers, convert them to floats --
-    # this is important since we'll be doing a bunch of divisions
-    if boxes.dtype.kind == "i":
-        boxes = boxes.astype("float")
-    # initialize the list of picked indexes	
-    pick = []
-    # grab the coordinates of the bounding boxes
-    y1 = boxes[:,0]
-    x1 = boxes[:,1]
-    y2 = boxes[:,2]
-    x2 = boxes[:,3]
-    # compute the area of the bounding boxes and sort the bounding
-    # boxes by the bottom-right y-coordinate of the bounding box
-    area = (x2 - x1 + 1) * (y2 - y1 + 1)
-    idxs = np.argsort(y2)
-    # keep looping while some indexes still remain in the indexes
-    # list
-    while len(idxs) > 0:
-        # grab the last index in the indexes list and add the
-        # index value to the list of picked indexes
-        last = len(idxs) - 1
-        i = idxs[last]
-        pick.append(i)
-        # find the largest (x, y) coordinates for the start of
-        # the bounding box and the smallest (x, y) coordinates
-        # for the end of the bounding box
-        xx1 = np.maximum(x1[i], x1[idxs[:last]])
-        yy1 = np.maximum(y1[i], y1[idxs[:last]])
-        xx2 = np.minimum(x2[i], x2[idxs[:last]])
-        yy2 = np.minimum(y2[i], y2[idxs[:last]])
-        # compute the width and height of the bounding box
-        w = np.maximum(0, xx2 - xx1 + 1)
-        h = np.maximum(0, yy2 - yy1 + 1)
-        # compute the ratio of overlap
-        overlap = (w * h) / area[idxs[:last]]
-        # delete all indexes from the index list that have
-        idxs = np.delete(idxs, np.concatenate(([last],
-            np.where(overlap > overlapThresh)[0])))
-    # return only the bounding boxes that were picked using the
-    # integer data type
-    return (boxes[pick].astype("int"), scores[pick].astype("float"), classes[pick].astype("int"))
+def py_cpu_softnms(dets, sc, Nt=0.5, sigma=1, thresh=0.001, method=2):
+    """
+    py_cpu_softnms
+    :param dets:   boexs 坐标矩阵 format [y1, x1, y2, x2]
+    :param sc:     每个 boxes 对应的分数
+    :param Nt:     iou 交叠门限
+    :param sigma:  使用 gaussian 函数的方差
+    :param thresh: 最后的分数门限
+    :param method: 使用的方法
+    :return:       留下的 boxes 的 index
+    """
+
+    if len(dets) == 0:
+        return []
+    # indexes concatenate boxes with the last column
+    N = dets.shape[0]
+    indexes = np.array([np.arange(N)])
+    dets = np.concatenate((dets, indexes.T), axis=1)
+
+    # the order of boxes coordinate is [y1,x1,y2,x2]
+    y1 = dets[:, 0]
+    x1 = dets[:, 1]
+    y2 = dets[:, 2]
+    x2 = dets[:, 3]
+    scores = sc
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+
+    for i in range(N):
+        # intermediate parameters for later parameters exchange
+        tBD = dets[i, :].copy()
+        tscore = scores[i].copy()
+        tarea = areas[i].copy()
+        pos = i + 1
+
+        #
+        if i != N-1:
+            maxscore = np.max(scores[pos:], axis=0)
+            maxpos = np.argmax(scores[pos:], axis=0)
+        else:
+            maxscore = scores[-1]
+            maxpos = 0
+        if tscore < maxscore:
+            dets[i, :] = dets[maxpos + i + 1, :]
+            dets[maxpos + i + 1, :] = tBD
+            tBD = dets[i, :]
+
+            scores[i] = scores[maxpos + i + 1]
+            scores[maxpos + i + 1] = tscore
+            tscore = scores[i]
+
+            areas[i] = areas[maxpos + i + 1]
+            areas[maxpos + i + 1] = tarea
+            tarea = areas[i]
+
+        # IoU calculate
+        xx1 = np.maximum(dets[i, 1], dets[pos:, 1])
+        yy1 = np.maximum(dets[i, 0], dets[pos:, 0])
+        xx2 = np.minimum(dets[i, 3], dets[pos:, 3])
+        yy2 = np.minimum(dets[i, 2], dets[pos:, 2])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        ovr = inter / (areas[i] + areas[pos:] - inter)
+
+        # Three methods: 1.linear 2.gaussian 3.original NMS
+        if method == 1:  # linear
+            weight = np.ones(ovr.shape)
+            weight[ovr > Nt] = weight[ovr > Nt] - ovr[ovr > Nt]
+        elif method == 2:  # gaussian
+            weight = np.exp(-(ovr * ovr) / sigma)
+        else:  # original NMS
+            weight = np.ones(ovr.shape)
+            weight[ovr > Nt] = 0
+
+        scores[pos:] = weight * scores[pos:]
+
+    # select the boxes and keep the corresponding indexes
+    inds = dets[:, 4][scores > thresh]
+    keep = inds.astype(int)
+
+    return keep
 
 if __name__ == "__main__":
     main()
